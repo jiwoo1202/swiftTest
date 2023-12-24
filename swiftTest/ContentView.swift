f99c5588b2c22020061b44eb17317d434967f741
heojiu <jiwoo12021@naver.com>
2023-12-24 15:54:28 +0900

    init

--- swiftTest/ContentView.swift
+++ swiftTest/ContentView.swift
@@ -4,16 +4,17 @@
 //
 //  Created by heojiwoo on 12/24/23.
 //
-
 import SwiftUI
-
 struct ContentView: View {
+    
+    var name : String = "리이오"
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(name)님 안녕하세요")
         }
         .padding()
     }
