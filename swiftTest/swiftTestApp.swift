a84770ee9a773aff477653a9b7f109aec7625ef3
heojiu <jiwoo12021@naver.com>
2023-12-24 14:21:41 +0900

    Initial Commit

---
+++ swiftTest/swiftTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftTestApp.swift
+//  swiftTest
+//
+//  Created by heojiwoo on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
