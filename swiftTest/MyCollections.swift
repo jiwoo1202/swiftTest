38d20ea37e81f2870699586dd3a936b47ef3400c
heojiu <jiwoo12021@naver.com>
2023-12-26 23:41:03 +0900

    12/26

--- swiftTest/MyCollections.swift
+++ swiftTest/MyCollections.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct MyCollections: View {
+    
+    var food : [String] = ["egg","banana","bean"] /// List
+    var jazzs : Set<String> = ["la","dudu","dids"] /// set
+    var hiphop : Set<String> = ["la","rap","wow"] /// set
+    var maps : [String: String] = ["id": "1234", "pw":"123"] /// map
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text(food[0])
+            Text(" hi \(hiphop.intersection(jazzs).description)")
+            Button(
+                action:{
+                    var interaction = hiphop.intersection(jazzs).description
+                }, label: {
+                Text("hit")
+            })
+            
+            Text(maps["id"]!)
+        }
     }
 }
 
