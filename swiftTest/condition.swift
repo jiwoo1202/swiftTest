a875ee3e3e94979e9eb9c88e21cc5a7ad1f89ca0
heojiu <jiwoo12021@naver.com>
2023-12-27 00:24:09 +0900

    12/27

---
+++ swiftTest/condition.swift
@@ -0,0 +1,18 @@
+//
+//  condition.swift
+//  swiftTest
+//
+//  Created by heojiwoo on 12/26/23.
+//
+
+import SwiftUI
+
+struct condition: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    condition()
+}
---
+++ swiftTest/function.swift
@@ -0,0 +1,18 @@
+//
+//  function.swift
+//  swiftTest
+//
+//  Created by heojiwoo on 12/27/23.
+//
+
+import SwiftUI
+
+struct function: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    function()
+}
