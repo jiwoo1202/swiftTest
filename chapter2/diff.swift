12469bdfa823f8ef66bc15da0c70209031bae69d
heojiu <jiwoo12021@naver.com>
2023-12-27 23:47:31 +0900

    12/27 구조체/클래스

---
+++ chapter2/diff.swift
@@ -0,0 +1,18 @@
+//
+//  diff.swift
+//  swiftTest
+//
+//  Created by heojiwoo on 12/27/23.
+//
+
+import SwiftUI
+
+struct diff: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    diff()
+}
