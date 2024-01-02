dd172215d7d766909e0883d6072273040c742a16
heojiu <jiwoo12021@naver.com>
2024-01-03 01:06:18 +0900

    2024/1/3

---
+++ chapter2/Switch.swift
@@ -0,0 +1,18 @@
+//
+//  Switch.swift
+//  swiftTest
+//
+//  Created by heojiwoo on 1/3/24.
+//
+
+import SwiftUI
+
+struct Switch: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Switch()
+}
