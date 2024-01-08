720ba69a72f0aed55bb9c8a96ac8ee762601b2e2
heojiu <jiwoo12021@naver.com>
2024-01-08 23:03:31 +0900

    2024/1/8

---
+++ chapter2/option.swift
@@ -0,0 +1,18 @@
+//
+//  option.swift
+//  swiftTest
+//
+//  Created by heojiwoo on 1/8/24.
+//
+
+import SwiftUI
+
+struct option: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    option()
+}
---
+++ chapter2/optionNull.swift
@@ -0,0 +1,18 @@
+//
+//  optionNull.swift
+//  swiftTest
+//
+//  Created by heojiwoo on 1/8/24.
+//
+
+import SwiftUI
+
+struct optionNull: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    optionNull()
+}
