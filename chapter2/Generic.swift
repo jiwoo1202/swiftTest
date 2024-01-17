4f4d528d709d232348cb8b4660d2ffa84a6a4a1b
heojiu <jiwoo12021@naver.com>
2024-01-18 00:49:37 +0900

    1/18(목)

---
+++ chapter2/Generic.swift
@@ -0,0 +1,18 @@
+//
+//  Generic.swift
+//  swiftTest
+//
+//  Created by heojiwoo on 1/18/24.
+//
+
+import SwiftUI
+
+struct Generic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Generic()
+}
