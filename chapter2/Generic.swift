532f80eba6bfebe75af9560a4fa6f21ff312d53b
heojiu <jiwoo12021@naver.com>
2024-01-18 00:50:31 +0900

    1/18(목)2

--- chapter2/Generic.swift
+++ chapter2/Generic.swift
@@ -8,11 +8,45 @@
 import SwiftUI
 
 struct Generic: View {
+    
+    @State var input = ""
+    @State var myStack = MyStack<Int>()
+//    @State var myStack = MyStack<Float>()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            TextField("숫자를 입력해주세요",text: $input)
+            Button(action: {
+                myStack.insertValue(input: Int(input) ?? 0)
+//                myStack.insertValue(input: Float(input) ?? 0.0)
+            }, label: {
+                Text("저장")
+            })
+            Button(action: {
+                myStack.showData()
+            }, label: {
+                Text("출력")
+            })
+        }
     }
 }
 
-#Preview {
-    Generic()
+struct MyStack<T>{
+    var data : [T] = []
+    
+   mutating func insertValue(input : T){
+        data.append(input)
+    }
+    
+    func showData(){
+        data.forEach { 
+            item in print(item)
+        }
+    }
+}
+
+struct Generic_Preview : PreviewProvider{
+    static var previews: some View{
+        Generic()
+    }
 }
