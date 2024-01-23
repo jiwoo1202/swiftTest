3b3f961ef211e247d66c5665005e877c45dae827
heojiu <jiwoo12021@naver.com>
2024-01-24 01:14:15 +0900

    1/24

--- WarmUp/02/ListLoop.swift
+++ WarmUp/02/ListLoop.swift
@@ -7,9 +7,54 @@
 
 import SwiftUI
 
+struct Fruit : Hashable{
+    let name : String
+    let matchFruitName :String
+    let price : Int
+}
+
 struct ListLoop: View {
+    
+    @State var fruitName: String = ""
+    @State var favoriteFruit = [
+        Fruit(name: "Apple", matchFruitName: "Banana", price: 1000),
+        Fruit(name: "Banana", matchFruitName: "Banana", price: 2000),
+        Fruit(name: "Cherry", matchFruitName: "DoubleKiwi", price: 3000)
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack{
+                HStack{
+                    TextField("insert Fruit name", text: $fruitName)
+                    Button(action: {
+                        favoriteFruit.append(Fruit(name: fruitName, matchFruitName: "Apple", price: 1000))
+                    }, label: {
+                        Text("insert")
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(.blue)
+                            .cornerRadius(10)
+                            
+                    })
+                }
+                .padding()
+                List{
+                    ForEach(favoriteFruit, id: \.self, content: { fruits in
+                        VStack(alignment: .leading){
+                            Text("name : \(fruits.name)")
+                            Text("matchFruit : \(fruits.matchFruitName)")
+                            Text("price : \(fruits.price)")
+                        }
+                        
+                    }).onDelete(perform: { index in
+                        favoriteFruit.remove(atOffsets: index)
+                    })
+                }
+                .navigationTitle("Fruit List")
+            }
+        }
+        
     }
 }
 
