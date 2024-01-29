676476ebee1fa5d75eae732d5ebd74435e75bbce
heojiu <jiwoo12021@naver.com>
2024-01-29 23:55:55 +0900

    1/29(월)

---
+++ WarmUp/02/Setting.swift
@@ -0,0 +1,18 @@
+//
+//  Setting.swift
+//  swiftTest
+//
+//  Created by heojiwoo on 1/28/24.
+//
+
+import SwiftUI
+
+struct Setting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Setting()
+}
---
+++ WarmUp/03/Modal.swift
@@ -0,0 +1,18 @@
+//
+//  Modal.swift
+//  swiftTest
+//
+//  Created by heojiwoo on 1/29/24.
+//
+
+import SwiftUI
+
+struct Modal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Modal()
+}
